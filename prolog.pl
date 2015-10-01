33221b4c1d61de4361813c9bc81ddc9c1085c8ec
Uzun Igor <13guff13@gmail.com>
2015-10-01 19:38:22 +0300

    added some temporaray and not temporary files. :)

---
+++ prolog.pl
@@ -0,0 +1,49 @@
+?- consult('prolog-db').
+ 
+ 
+?- likes(george, susie).
+
+
+?- listing(likes).
+
+    ?-trace(likes, -all).
+
+
+
+
+?- assert(likes(david, sarah)).
+
+
+
+
+
+
+
+
+    told
+    seen
+    respective
+    originating
+    number
+    clause/clauses
+    uses
+    regardless
+    accomplished
+    facilities
+    satisfy
+    rather
+    retry
+    particular
+    primary
+    control
+    consider
+    indeed
+    process
+    unification
+    commas
+    besides
+
+    attempt
+    computation
+    soon
+
